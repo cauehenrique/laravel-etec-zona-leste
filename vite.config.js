94830a7a3e574c459d7f13512ec0d950e62037a0
jpmanaresi <jpmanaresi@gmail.com>
2022-08-02 10:53:49 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    esbuild: {
+        loader: 'jsx'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx'
+            }
+        }
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        react(),
+    ],
+});
