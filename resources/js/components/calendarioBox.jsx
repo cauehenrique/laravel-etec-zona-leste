bec18902383178164cd40707e32142891bb4d633
AlexsanderBenatti <abenatti.silva@gmail.com>
2022-11-25 16:24:53 -0300

    Calendarios page done

---
+++ resources/js/components/calendarioBox.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function calendarioBox({ title, link, image }) {
+	return (
+		<a
+			href={link}
+			target="_blank"
+			className="w-5/12 flex rounded-lg border items-center"
+		>
+			<img
+				src={image}
+				alt="Sede"
+				className="h-32 w-48 rounded-tl-lg rounded-bl-lg object-cover"
+			/>
+			<div className="p-8 flex flex-col">
+				<h1 className="text-lg font-semibold">{title}</h1>
+			</div>
+		</a>
+	)
+}
--- resources/js/pages/calendario.jsx
+++ resources/js/pages/calendario.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import Sidebar from "../components/Sidebar"
 import TopBar from "../components/TopBar"
 import Footer from "../components/Footer"
+import CalendarioBox from "../components/calendarioBox"
 
 export default function Calendario() {
 	return (
@@ -11,6 +12,33 @@ export default function Calendario() {
 				<Sidebar />
 				<div className="flex flex-col w-full">
 					<TopBar />
+					<div className="py-6 p-24 flex flex-col w-full items-center">
+						<h1 className="text-3xl font-bold mb-12">
+							CALENDÁRIOS 2022 - SEDE E EXTENSÕES
+						</h1>
+						<div className="flex flex-col justify-center items-center space-y-8 w-full">
+							<CalendarioBox
+								title="ETEC ZL - SEDE"
+								link="https://drive.google.com/file/d/1KBMUFrEKeJzc12xyNgHIa43g6YZ0KE57/view"
+								image="https://bkpsitecpsnew.blob.core.windows.net/uploadsitecps/sites/1/2020/11/portas_etec_zl2.jpg"
+							/>
+							<CalendarioBox
+								title="Extensão CEU Azul da Cor do Mar"
+								link="https://drive.google.com/file/d/1MBfP4V4xVOIBBJPpUxQzGAIxwiwTo3Pw/view"
+								image="http://portal.sme.prefeitura.sp.gov.br/Portals/1/Images/CEUs_fachadas/1126.jpg"
+							/>
+							<CalendarioBox
+								title="Extensão CEU Pq. São Carlos"
+								link="https://drive.google.com/file/d/1bwZf5tWXnehIczsl6y99QxlPgeKZ4Egy/view"
+								image="https://payload213.cargocollective.com/1/12/399321/6568244/ceu_butanta_o.jpg"
+							/>
+							<CalendarioBox
+								title="Extensão FATEC ZL"
+								link="https://drive.google.com/file/d/1H_MBAnxzveUt6zGUzhwXJreR82ECN7qz/view"
+								image="http://www.fateczl.edu.br/imagens/fatec1.png"
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 			<Footer />
