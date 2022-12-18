df55929930fd4d0e210548b390e804b7321d9e25
cauehenrique <caue313@gmail.com>
2022-12-18 20:33:15 -0300

    fix: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite"
+import laravel from "laravel-vite-plugin"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        react(),
-    ],
-});
+	plugins: [
+		laravel({
+			input: ["resources/css/app.css", "resources/js/index.js"],
+			refresh: true,
+		}),
+		react(),
+	],
+})
